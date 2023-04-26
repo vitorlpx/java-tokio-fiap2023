2e0ca979709f807da66a8c56e5df987b5ecfd941
vitorlpx <vitorlpx>
2023-04-26 18:20:03 -0300

    Inicialização dos exercícios da aula 07.

--- JavaTokio2023-1/src/logica/exercicio/aula07/Exercicio01.java
+++ JavaTokio2023-1/src/logica/exercicio/aula07/Exercicio01.java
@@ -1,10 +1,31 @@
 package logica.exercicio.aula07;
 
+import java.util.Scanner;
+
 public class Exercicio01 {
 
 	public static void main(String[] args) {
 		
+		//▪ Faça um programa que exiba a mensagem “Olá, Mundo”.
+		//▪ Essa mensagem deverá ser exibida repetidamente.
+		//▪ Ao final de toda iteração da repetição, você deve perguntar ao usuário se ele deseja exibir a mensagem novamente.
+		//▪ Se sim, exiba novamente. Senão, saia do loop e exiba a mensagem “Fim”.
 		
+		int i = 0;
+		String msg;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Olá, Mundo!");
+		
+		do {
+			System.out.println("Deseja repetir a mensagem acima? (y/n)");
+			msg = sc.nextLine();
+			i++;
+		}
+		while (msg.equals("y"));
+		
+		System.out.println("Você repetiu " + i + " vezes. \nFim.");
 
 	}
 
---
+++ JavaTokio2023-1/src/logica/exercicio/aula07/Exercicio02.java
@@ -0,0 +1,20 @@
+package logica.exercicio.aula07;
+
+public class Exercicio02 {
+
+	public static void main(String[] args) {
+		
+//		▪ Contagem de 0 a 100 pulando de 10 em 10.
+//		▪ O terminal deve ficar assim:	0, 10, 20, 30, 40, 50, 60, 70, 80, 90 e 100.
+		
+		int i = 0;
+		
+		for(i = 0; i <=100; i+=10) {
+	
+			System.out.println(i);
+			
+		}
+
+	}
+
+}
